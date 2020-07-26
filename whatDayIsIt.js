78c810245b53be0ab705db5a3dd3140973e6926f
Aditya Gupta <ag15035@gmail.com>
2020-07-26 22:55:10 +0530

    Day 10 - Read abt GridBox with B

---
+++ whatDayIsIt.js
@@ -0,0 +1,7 @@
+const theStart17 = 1594924200000
+
+let now = ( Date.now() - theStart17 )/(1000*3600*24)
+
+console.log("Completed", now, 'days')
+console.log("Today is Day", Math.trunc(now) + 1)
+console.log("Tommorow will be Day", Math.trunc(now) + 2)
